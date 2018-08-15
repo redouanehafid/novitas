439f9561d35235f667a59ceac7e637aaabe388bd
Marouan <mailmarouan@gmail.com>
2018-08-15 19:01:46 +0100

    delete confirmable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.where(email: "admin@gmail.com").first_or_create(email: "admin@gmail.com" , password: "123456", admin: true, confirmed_at: Time.now)
