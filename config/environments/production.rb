098a3a726939315f556dd37c31e1e4a1f2963e80
redouane <redouaanehafid@gmail.com>
2018-09-20 20:29:34 +0100

    Addeding contact

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => 'gmail.com',
    :user_name            => 'contactnovitas@gmail.com',
    :password             => 'novitas@2018',
    :authentication       => "plain",
